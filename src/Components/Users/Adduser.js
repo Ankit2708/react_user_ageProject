304848f1ff86eecfea24f5af927833dd3966d425
Ankit2708 <69914264+Ankit2708@users.noreply.github.com>
2023-07-26 00:11:49 +0530

    reactportal added

--- src/Components/Users/Adduser.js
+++ src/Components/Users/Adduser.js
@@ -4,6 +4,7 @@ import Card from '../UI/Card';
 import Button from '../UI/Button';
 import classes from './Adduser.module.css';
 import ErrorModal from '../UI/ErrorModal';
+import Wrapper from '../Helpers/Wrapper';
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
@@ -40,7 +41,7 @@ const AddUser = (props) => {
     setError(null)
   }
   return (
-    <div>
+    <Wrapper>
     {error && 
     <ErrorModal title={error.title} 
     message={error.message}
@@ -64,7 +65,7 @@ const AddUser = (props) => {
         <Button type="submit">Add User</Button>
       </form>
     </Card>
-    </div>
+    </Wrapper>
   );
 };
 
